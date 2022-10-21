ef9e619ad6d068a617948047f29a474eac0f1020
Miriam <miriamjoshua00@gmail.com>
2022-10-20 21:35:02 -0400

    installed and seeded with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-donors = Donor.create([{ name: "Ben" }, { name: "Avi" }, { name: "Rachel" }])
-donations = Donation.create([{ amount: 100 }, { amount: 50 }, { amount: 1 }])
-recipients = Recipient.create([{ name: "Help the Hungry" }, { name: "Medical Research" }, { name: "Education for All" }])
+# donors = Donor.create([{ name: "Ben" }, { name: "Avi" }, { name: "Rachel" }])
+# donations = Donation.create([{ amount: 100 }, { amount: 50 }, { amount: 1 }])
+# recipients = Recipient.create([{ name: "Help the Hungry" }, { name: "Medical Research" }, { name: "Education for All" }])
+
+Donor.create(name: Faker::Name.name_with_middle)
+Donor.create(name: Faker::Name.name_with_middle)
+Donor.create(name: Faker::Name.name_with_middle)
+
+Donation.create(amount: Faker::Number.decimal(l_digits: 2))
+Donation.create(amount: Faker::Number.decimal(l_digits: 2))
+Donation.create(amount: Faker::Number.decimal(l_digits: 2))
+
+Recipient.create(name: Faker::University.name)
+Recipient.create(name: Faker::University.name)
+Recipient.create(name: Faker::University.name)
